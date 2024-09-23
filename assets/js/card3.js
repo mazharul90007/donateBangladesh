536227bfbda40cadce48d5237e9d692a345b03d5
mazharul90007 <mazharul90007@gmail.com>
2024-09-23 19:29:43 +0600

    card2 and card3 added and dynamic part done

---
+++ assets/js/card3.js
@@ -0,0 +1,50 @@
+document.getElementById('donate-btn3').addEventListener('click', () => {
+    //get value from input field
+    const donationAmount = getInputValueById('donationAmount3');
+    //get value from element
+    const totalDonationValue = getElementValueById('totalDonation3');
+    //get total remaining money
+    const remainingMoney = getElementValueById('remainingMoney');
+    //get card Title
+    const cardTitle = getInnerTextById('card-title3');
+    //get time
+    const now = new Date();
+    const time = now.toLocaleString();
+
+    if (donationAmount > 0) {
+        if (donationAmount > remainingMoney) {
+            alert('Sorry! Insufficient Balance');
+        } else {
+            //add amount in the card donation section
+            document.getElementById('totalDonation3').innerText = totalDonationValue + donationAmount;
+            //subtract from remaining Money
+            document.getElementById('remainingMoney').innerText = remainingMoney - donationAmount;
+            //clear donationAmount input field
+            document.getElementById('donationAmount3').value = "";
+
+            //create history card
+            const historyCard = document.createElement('div');
+
+            historyCard.innerHTML =
+                `<div class=" border border-gray-300 p-4 rounded-lg shadow-lg bg-lime-50 mb-3 ">
+                    <h3 class="text-xl md:text-2xl font-semibold"><span class = "text-green-700">${donationAmount} </span>Taka is Donated for ${cardTitle}</h3> 
+                    <p class =" text-gray-500 text-[16px] md:text-xl">Donate Time: ${time}</p>
+                </div>`;
+
+            const history = document.getElementById('history');
+            history.appendChild(historyCard);
+            //show Modal
+            modal.classList.remove('hidden');
+            //Close Modal
+            const closeModal = document.getElementById('closeModal');
+            closeModal.addEventListener('click', ()=>{
+                modal.classList.add('hidden');
+            });
+        }
+
+    }
+    else {
+        alert('Please enter a valid Number')
+    }
+})
+
--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,17 +1,16 @@
 //show Donation
 document.getElementById('donation-button').addEventListener('click', ()=>{
     showSection('donation-section');
+    document.getElementById('donation-button').classList.add("bg-lime-300");
+    document.getElementById('history-button').classList.remove("bg-lime-300");
 })
 //show History
 document.getElementById('history-button').addEventListener('click', ()=>{
     showSection('history-section');
+    document.getElementById('donation-button').classList.remove("bg-lime-300");
+    document.getElementById('history-button').classList.add("bg-lime-300");
 })
 
 document.getElementById('blog').addEventListener('click', ()=>{
     window.location.href = '/blog.html';
 })
-
-// document.getElementById('home').addEventListener('click', ()=>{
-//     console.log('fuck you')
-//     window.location.href = '/index.html';
-// })
